27f2d85e75f051a69d85831d0b5edb8c217c4219
joycode <hcwang@joycode.com>
2025-10-01 06:41:56 +0800

    Update homepage and URL for Vision formula

--- Formula/vision.rb
+++ Formula/vision.rb
@@ -1,7 +1,7 @@
 class Vision < Formula
   desc "图片文字识别命令行工具"
-  homepage "https://github.com/hcwang/homebrew"
-  url "https://github.com/hcwang/homebrew/releases/download/v1.0.0/vision.zip"
+  homepage "https://github.com/hcwang/homebrew-mactools"
+  url "https://github.com/hcwang/homebrew-mactools/releases/download/v1.0.0/vision.zip"
   sha256 "d0327d3a7d12a2a2df372946c7650443b319a0dc8a89e1f22212c7184a106b9c"
   version "1.0.0"
 
